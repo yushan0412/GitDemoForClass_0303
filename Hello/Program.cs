d78b036fbab03f76c2e7fb1ed9b23e4ad2416a9e
yushan0412 <014067@stu.chsc.tw>
2017-03-03 14:22:00 +0800

    fdfdfd

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -17,6 +17,7 @@ namespace Hello
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //fdfdfdf
         }
     }
 }
